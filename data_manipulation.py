9960604fefdd96a252bdab6ad01316d36ef10a36
Šimon Sebechlebský <simon.sebechlebsky@gmail.com>
2019-12-06 17:29:26 +0100

    MORE AUGMENTATION

--- data_manipulation.py
+++ data_manipulation.py
@@ -128,11 +128,12 @@ def get_split_data_lists():
 def get_image_generators(config):
     train_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255,
                                                                  horizontal_flip=True,
-                                                                 rotation_range=25,
+                                                                 rotation_range=30,
                                                                  shear_range=10,
                                                                  width_shift_range=0.15,
                                                                  height_shift_range=0.15,
-                                                                 channel_shift_range=10
+                                                                 zoom_range=0.2,
+                                                                 channel_shift_range=0.2
                                                                  )
     train_generator = train_datagen.flow_from_directory(
         os.path.join(DATA_DIR, 'train'),
